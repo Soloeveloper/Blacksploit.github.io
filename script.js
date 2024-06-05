01f31de4ec4a6ea558af7902b76bbe12e70a2dee
Soloeveloper <159730900+Soloeveloper@users.noreply.github.com>
2024-06-05 17:20:34 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.getElementById("download-btn").addEventListener("click", function() {
-    window.location.href = "Downloads/BlackSploit.zip"; // Change the path to your zip file
+    window.location.href = "Downloads/BlackSploit.zip";
 });
 
 document.getElementById("contact-btn").addEventListener("click", function() {
     const email = "Exprvssed@gmail.com";
     navigator.clipboard.writeText(email).then(function() {
-        alert("Email copied to clipboard: " + email);
+        alert("Copied Email  " + email);
     }, function() {
-        alert("Failed to copy email to clipboard. Please copy it manually: " + email);
+        alert("Failed Try Again  " + email);
     });
 });
