e366aad68f5b9b69cb8318b4b754fbc59eef3cef
Soloeveloper <159730900+Soloeveloper@users.noreply.github.com>
2024-06-07 12:37:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 document.getElementById('download-btn').addEventListener('click', function() {
-    window.location.href = 'Downloads/BlackSploit.zip'; // Adjust the path to your file
+    window.location.href = 'Downloads/BlackSploit.zip';
 });
 
 document.getElementById('contact-btn').addEventListener('click', function() {
